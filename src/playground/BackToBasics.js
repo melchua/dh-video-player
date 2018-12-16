2976ec88b27072a57d5cf12e175d0ec7dd327185
Mel <mchuam@gmail.com>
2018-12-16 01:48:35 -0800

    added some basic controls

--- src/playground/BackToBasics.js
+++ src/playground/BackToBasics.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
 import Fullscreenable from 'react-fullscreenable';
 import { withSize } from 'react-sizeme';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import Button from '@material-ui/core/Button';
+
+
 
 const withSizeWrapper = withSize({ monitorHeight: true });
 
@@ -17,12 +22,15 @@ class Player extends Component {
         }
     }
     render() {
+        const { playing, loop } = this.props;
         return (
             <div>
                 <ReactPlayer
                     url="https://res.cloudinary.com/dance-hive/video/upload/v1544581858/videos/DHDemo.mp4"
                     width="100%"
                     height="100%"
+                    playing={playing}
+                    loop={loop}
                 />
             </div>
         )    
@@ -34,11 +42,29 @@ const PlayerSizeAware = withSizeWrapper(Player);
 class BasicVideo extends Component {
     state = {
         vidWidth: 0,
-        vidHeight: 0
+        vidHeight: 0,
+        isFront: true,
+        isMirror: true,
+        playing: true,
+        loop: true
     }
     getPlayerSize = (sizeFromPlayer) => {
         this.setState({ vidHeight: sizeFromPlayer.height / 2});
         this.setState({ vidWidth: sizeFromPlayer.width});
+    }
+    
+    // handlers for controls
+    switchAngle = () => {
+        this.setState(({isFront}) => ({ isFront: !isFront }))
+    }
+    onMirror = () => {
+        this.setState(({mirror}) => ({mirror: !mirror}));
+    }
+    playPause = () => {
+        this.setState({ playing: !this.state.playing })
+    }
+    toggleLoop = () => {
+        this.setState({ loop: !this.state.loop })
     }
     componentDidUpdate() {
         // console.log(this.state.vidWidth);
@@ -53,18 +79,31 @@ class BasicVideo extends Component {
         }
         const mover = {
             position: 'absolute',
-            top: -this.state.vidHeight,
+            top: this.state.isFront ?  0 : -this.state.vidHeight,
+            transform: this.state.mirror ? 'rotateY(180deg)' : 'rotateY(0deg)',
             width: '100%',
             height: '100%', // height needs to be a number that comes from state and child component
             backgroundColor: 'black',
         }
+        const buttonStyle = {
+            color: 'white',
+            // zIndex: 10,
+            // height: '10px'
+        }
+        const {isFront, onMirror, playing, loop} = this.state;
         return (
             <div style={bigassWrapper}>
                 <div style={mover} className="mover">
-                    <PlayerSizeAware triggerGetPlayerSize={this.getPlayerSize}/>
-                    {/* <div style={{position:'absolute', top: '300px'}}>
-                        <button onClick={this.props.toggleFullscreen}>Fullscreen</button>
-                    </div> */}
+                    <PlayerSizeAware triggerGetPlayerSize={this.getPlayerSize} playing={playing} loop={loop}/>
+                </div>
+                <div style={{position:'absolute', bottom: '0px'}}>
+                    <button onClick={this.props.toggleFullscreen}>Fullscreen</button>
+                    <Button style={buttonStyle} onClick={this.playPause}>{playing ? 'Pause' : 'Play'}</Button>
+                    <FormControlLabel style={buttonStyle} control={<Switch onChange={this.switchAngle} checked={isFront} />} 
+                        label={isFront ? <span style={{color: 'white'}}>Front</span> : <span style={{color: 'white'}}>Back</span>} />
+                    <FormControlLabel style={buttonStyle} control={<Switch onChange={this.onMirror} checked={onMirror}/>} label={<span style={{color: 'white'}}>Mirror</span>} />
+                    <FormControlLabel style={buttonStyle} control={<Switch onChange={this.toggleLoop} checked={loop}/>} 
+                        label={<span style={{color: 'white'}}>Loop</span>} />
                 </div>
             </div>
         )
