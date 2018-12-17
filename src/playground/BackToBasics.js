65d2fca49bd78e56b32f6469804c7c78add9be0a
Mel <mchuam@gmail.com>
2018-12-16 17:57:27 -0800

    added progress bar

--- src/playground/BackToBasics.js
+++ src/playground/BackToBasics.js
@@ -10,17 +10,20 @@ import Pause from '@material-ui/icons/Pause';
 import Loop from '@material-ui/icons/Loop';
 import Fullscreen from '@material-ui/icons/Fullscreen'; 
 import FullscreenExit from '@material-ui/icons/FullscreenExit'; 
-
-
-
+import VolumeUp from '@material-ui/icons/VolumeUp'; 
+import VolumeOff from '@material-ui/icons/VolumeOff'; 
+import Slider from '@material-ui/lab/Slider';
 
 const withSizeWrapper = withSize({ monitorHeight: true });
 
 class Player extends Component {
+    playerRef = React.createRef();
     updatePlayerSize = () => {
         this.props.triggerGetPlayerSize(this.props.size);        
     }
     componentDidMount() {
+        console.log(this.playerRef);
+        this.props.onRef(this);
     }
     componentDidUpdate(prevProps) {
         if (JSON.stringify(this.props.size) !== JSON.stringify(prevProps.size)) { 
@@ -28,15 +31,17 @@ class Player extends Component {
         }
     }
     render() {
-        const { playing, loop } = this.props;
+        const { playing, loop, onProgress } = this.props;
         return (
             <div>
                 <ReactPlayer
+                    ref={this.playerRef}
                     url="https://res.cloudinary.com/dance-hive/video/upload/v1544581858/videos/DHDemo.mp4"
                     width="100%"
                     height="100%"
                     playing={playing}
                     loop={loop}
+                    onProgress={onProgress}
                 />
             </div>
         )    
@@ -52,8 +57,11 @@ class BasicVideo extends Component {
         isFront: true,
         isMirror: true,
         playing: true,
-        loop: true
+        loop: true,
+        played: 0,
+        seeking: false
     }
+    
     getPlayerSize = (sizeFromPlayer) => {
         this.setState({ vidHeight: sizeFromPlayer.height / 2});
         this.setState({ vidWidth: sizeFromPlayer.width});
@@ -67,14 +75,40 @@ class BasicVideo extends Component {
         this.setState(({mirror}) => ({mirror: !mirror}));
     }
     playPause = () => {
-        this.setState({ playing: !this.state.playing })
+        this.setState(({playing}) =>  ({ playing: !playing }))
     }
     toggleLoop = () => {
-        this.setState({ loop: !this.state.loop })
+        this.setState(({loop}) =>  ({ loop: !loop }))
+    }
+    toggleMuted = () => {
+        this.setState(({muted}) => ({ muted: !muted }));
+    }
+    handleSliderValueChange = (e, value) => {
+        this.setState({ played: value});
+        console.log('on change value: ', value);
+        this.child.playerRef.current.seekTo(parseFloat(value));
+    }
+    onSeekMouseDown = e => {
+        this.setState({ seeking: true })
+    }
+    onSeekChange = e => {
+        this.setState({ played: parseFloat(e.target.value) })
+    }
+    onSeekMouseUp = e => {
+        this.setState({ seeking: false })
+        this.child.playerRef.current.seekTo(parseFloat(e.target.value))
+    }
+    onProgress = state => {
+        console.log('onProgress', state)
+        // We only want to update time slider if we are not currently seeking
+        if (!this.state.seeking) {
+            this.setState(state)
+        }
     }
     componentDidUpdate() {
         // console.log(this.state.vidWidth);
         // console.log(this.state.vidHeight);
+        console.log(this.state.played);
     }
     render() {
         const bigassWrapper = {
@@ -96,23 +130,54 @@ class BasicVideo extends Component {
             // zIndex: 10,
             // height: '10px'
         }
-        const {isFront, onMirror, playing, loop} = this.state;
+        const controlBarStyle = {
+            backgroundColor: 'rgba(0,0,0,0.4)',
+            position: 'absolute',
+            bottom: '0px',
+            width: '100%'
+        }
+        const {isFront, onMirror, playing, loop, muted, played} = this.state;
         const {isFullscreen, toggleFullscreen} = this.props;
+
         return (
             <div style={bigassWrapper}>
                 <div style={mover} className="mover">
-                    <PlayerSizeAware triggerGetPlayerSize={this.getPlayerSize} playing={playing} loop={loop}/>
+                    <PlayerSizeAware onRef={ref => (this.child = ref)} triggerGetPlayerSize={this.getPlayerSize} playing={playing} loop={loop} onProgress={this.onProgress}/>
                 </div>
-                <div style={{position:'absolute', bottom: '0px'}}>
-                    <button onClick={this.props.toggleFullscreen}>Fullscreen</button>
-                    <Button style={buttonStyle} onClick={this.playPause}>{playing ? <Pause /> : <PlayArrow />}</Button>
-                    <Button style={buttonStyle} onClick={this.toggleLoop}>{loop ? <Loop style={{color: 'red'}} /> : <Loop style={{color: 'white'}} />}</Button>
-                    <Button style={buttonStyle} onClick={toggleFullscreen}>{isFullscreen ? <FullscreenExit style={{color: 'white'}} /> : <Fullscreen style={{color: 'white'}} />}</Button>
+                <div style={controlBarStyle}>
+                    <div style={{width: '100%'}}>
+                        {/* <Slider
+                            value={played}
+                            aria-labelledby="label"
+                            onChange={this.handleSliderValueChange}
+                            // onMouseDown={this.onSeekMouseDown}
+                            // onChange={this.onSeekChange}
+                            // onMouseUp={this.onSeekMouseUp}
+                        /> */}
 
-                    <FormControlLabel style={buttonStyle} control={<Switch onChange={this.switchAngle} checked={isFront} />} 
-                        label={isFront ? <span style={{color: 'white'}}>Front</span> : <span style={{color: 'white'}}>Back</span>} />
-                    <FormControlLabel style={buttonStyle} control={<Switch onChange={this.onMirror} checked={onMirror}/>} label={<span style={{color: 'white'}}>Mirror</span>} />
+                        <input 
+                            style={{width: '100%'}}
+                            type="range" 
+                            min={0} 
+                            max={1} 
+                            step="any"
+                            value={played}
+                            onMouseDown={this.onSeekMouseDown}
+                            onChange={this.onSeekChange}
+                            onMouseUp={this.onSeekMouseUp}
+                        />
 
+                    </div>
+                    <div>
+                        <Button style={buttonStyle} onClick={this.playPause}>{playing ? <Pause /> : <PlayArrow />}</Button>
+                        <Button style={buttonStyle} onClick={this.toggleLoop}>{loop ? <Loop style={{color: 'red'}} /> : <Loop style={{color: 'white'}} />}</Button>
+                        <Button style={buttonStyle} onClick={this.toggleMuted}>{muted ? <VolumeOff style={{color: 'white'}} /> : <VolumeUp style={{color: 'white'}} />}</Button>
+                    
+                        <FormControlLabel style={buttonStyle} control={<Switch onChange={this.switchAngle} checked={isFront} />} 
+                            label={isFront ? <span style={{color: 'white'}}>Front</span> : <span style={{color: 'white'}}>Back</span>} />
+                        <FormControlLabel style={buttonStyle} control={<Switch onChange={this.onMirror} checked={onMirror}/>} label={<span style={{color: 'white'}}>Mirror</span>} />
+                        <Button style={buttonStyle} onClick={toggleFullscreen}>{isFullscreen ? <FullscreenExit style={{color: 'white'}} /> : <Fullscreen style={{color: 'white'}} />}</Button>
+                    </div>
                 </div>
             </div>
         )
